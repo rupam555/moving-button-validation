ce2754a3369c1227e199ba9b08bf85d0444accd3
RUPAM CHAKRABORTY <rupamchakraborty812@gmail.com>
2023-07-07 00:10:11 +0530

    1st commit

---
+++ main.js
@@ -0,0 +1,54 @@
+
+var a = 0;
+
+function mouseOver() {
+
+    const name = document.forms['suForm']['name'].value;
+    const email = document.forms['suForm']['email'].value;
+    const pass = document.forms['suForm']['pass'].value;
+
+    const tick = document.querySelector('#check');
+
+    const emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if ((name == "" || !email.match(emailCheck) || pass == "" || tick.checked == false) && a == 0) {
+        buttonMoveLeft();
+        a = 1;
+        return false;
+    }
+
+    if ((name == "" || !email.match(emailCheck) || pass == "" || tick.checked == false) && a == 1) {
+        buttonMoveRight();
+        a = 2;
+        return false;
+    }
+
+    if ((name == "" || !email.match(emailCheck) || pass == "" || tick.checked == false) && a == 2) {
+        buttonMoveLeft();
+        a = 1;
+        return false;
+    }
+    else {
+        document.getElementById('submit-btn').style.cursor = 'pointer';
+        return false;
+    };
+};
+
+function buttonMoveLeft() {
+    const button = document.getElementById('submit-btn');
+    button.style.transform = 'translateX(-175%)';
+    button.style.background = 'red';
+};
+
+function buttonMoveRight() {
+    const button = document.getElementById('submit-btn');
+    button.style.transform = 'translateX(0%)';
+    button.style.background = 'red';
+};
+
+function resetBtn() {
+    const button = document.getElementById('submit-btn');
+    button.style.transform = 'translateX(-85%)';
+    button.style.background = 'green';
+};
+
